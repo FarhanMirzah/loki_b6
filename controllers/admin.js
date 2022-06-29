1f4f76aba9f476bdef1bd1e7c86c03d00ebf3eb7
NadillaSaraswati <90832058+NadillaSaraswati@users.noreply.github.com>
2022-06-30 02:25:50 +0700

    controller admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,5 +5,40 @@ const cookieParser = require('cookie-parser')
 
 const controllers = {}
 
+controllers.dashboardAdmin = async(req, res) => {
+    res.render("admin/dashboardAdmin")
+}
+
+controllers.editDosenPengampu = async(req, res) => {
+    res.render("admin/Pemrograman_Web/editDosenPengampu")
+}
+
+controllers.metodeRPS = async(req, res) => {
+    res.render("admin/Pemrograman_Web/metodeRPS")
+}
+
+controllers.petaCPLCPMK = async(req, res) => {
+    res.render("admin/Pemrograman_Web/petaCPL-CPMK")
+}
+
+controllers.tambahDosenPengampu = async(req, res) => {
+    res.render("admin/Pemrograman_Web/tambahDosenPengampu")
+}
+
+controllers.editDosenPengampu = async(req, res) => {
+    res.render("admin/Rekayasa_Perangkat_Lunak/editDosenPengampu")
+}
+
+controllers.metodeRPS = async(req, res) => {
+    res.render("admin/Rekayasa_Perangkat_Lunak/metodeRPS")
+}
+
+controllers.petaCPLCPMK = async(req, res) => {
+    res.render("admin/Rekayasa_Perangkat_Lunak/petaCPL-CPMK")
+}
+
+controllers.tambahDosenPengampu = async(req, res) => {
+    res.render("admin/Rekayasa_Perangkat_Lunak/tambahDosenPengampu")
+}
 
 module.exports = controllers
